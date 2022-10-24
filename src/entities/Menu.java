a40faa0867e34d4b075a809d85b15935d01de0a9
Marco De Fenzo <82841719+Kram037@users.noreply.github.com>
2022-10-24 11:15:16 +0200

    fix: restart Menu

--- src/entities/Menu.java
+++ src/entities/Menu.java
@@ -2,21 +2,5 @@ package entities;
 
 public class Menu {
 
-    public String name;
-    public String ingredients;
-    public double price;
-    private Menu instance = new Menu();
-
-    private Menu(){}
-
-    protected Menu(String name, String ingredients, double price){
-        this.name = name;
-        this.ingredients = ingredients;
-        this.price = price;
-    }
-
-    public static Menu getMenu(){
-        return instance;
-    }
-
+ 
 }
