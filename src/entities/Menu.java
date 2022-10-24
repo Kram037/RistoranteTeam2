4e21061ec014416cb4ddba5a590ebaf83f08b110
Marco De Fenzo <82841719+Kram037@users.noreply.github.com>
2022-10-24 11:34:08 +0200

    feat: added Menu class

--- src/entities/Menu.java
+++ src/entities/Menu.java
@@ -1,6 +1,44 @@
 package entities;
+import java.util.List;
 
 public class Menu {
-
+     
+    private String restaurantName;
+    
+    private List<Dish> appetizers;
+    private List<Dish> mainCourses;
+    private List<Dish> secondCourses;
+    private List<Dish> desserts;
+    private List<Dish> drinks;
+ 
+    public Menu(){
+        this.restaurantName = "I Secondini";
+    }
+ 
+    public void addAppetizer(Appetizer appetizer){
+       appetizers.add(appetizer);
+    }
+ 
+    public void addMainCourse(MainCourse mainCourse){
+       mainCourses.add(mainCourse);
+    }
+ 
+     public void addSecondCourse(SecondCourse secondCourse){
+       secondCourses.add(secondCourse);
+    }
+ 
+     public void addDessert(Dessert dessert){
+       desserts.add(dessert);
+    }
+ 
+     public void addDrink(Drink drink){
+       drinks.add(drink);
+    }
+ 
+ 
+    public void getMenu(){
+        
+        System.out.println(restaurantName + appetizers + mainCourses + secondCourses + desserts + drinks);
+    }
  
 }
