e2a05b6c1ed5d37009ea16e697c3f0360b0df3a6
Vittoria Vassarotti <vit.vassarotti@outlook.it>
2022-10-26 12:36:00 +0200

    fix: fixed class Desserts, add desserts to Start

--- src/Start.java
+++ src/Start.java
@@ -1,6 +1,5 @@
+import entities.Desserts;
 import entities.Menu;
-import entities.MenuDolci;
-import entities.MenuAppetizers;
 import entities.Drinks;
 
 public class Start {
@@ -11,7 +10,11 @@ public class Start {
 
 
         menu.addDrink(new Drinks("Acqua Ferragni", " ", 7.50));
-        menu.getMenu();
-        
+
+        menu.addDessert(new Desserts("Crema catalana", "Latte, uova, zucchero, limone, cannella, amido di mais (maizena), zucchero di canna.", 10.50));
+        menu.addDessert(new Desserts("Budino al cioccolato con foglia oro", "Latte intero, cioccolato fondente, zucchero, burro, amido di mais (maizena)", 25.00));
+        menu.addDessert(new Desserts("Macedonia di frutta fresca, freschissima", "Banana, kiwi, fragole, mela verde, mandarino", 5));
+
+        menu.getMenu(); //spostato per chiamare tutto il menu completo, scrivete sopra!!
     }
 }
---
+++ src/entities/Desserts.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Desserts extends Dish{
+
+    public Desserts(String name, String ingredients, double price) {
+        super(name, ingredients, price);
+    }
+}
